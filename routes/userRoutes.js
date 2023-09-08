e90251cb8e0bcc196bacd35f72a84610649fb6a4
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-09-08 12:34:51 +0400

    add cruds : single user by id and get all users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,25 @@ router.get("/users", async (req, res) => {
   }
 });
 
-// ... Other CRUD operations for Users
+//Get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET a single user by ID
+router.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
