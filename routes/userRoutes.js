70e5d871a9aa366673e0443507c26d5507ea38b2
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-09-08 12:42:47 +0400

    add cruds : deletion ,update by userId and post new user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -32,4 +32,59 @@ router.get("/users/:id", async (req, res) => {
   }
 });
 
+//Post a new user
+router.post("/users", async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  const user = new User({
+    username: req.body.username,
+    password: hashedPassword,
+    role: req.body.role,
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// PUT - Update a user by ID
+
+router.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    if (req.body.username) {
+      user.username = req.body.username;
+    }
+    if (req.body.password) {
+      user.password = await bcrypt.hash(req.body.password, 10);
+    }
+    if (req.body.role) {
+      user.role = req.body.role;
+    }
+
+    const updatedUser = await user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE - Delete a user by ID
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    await user.remove();
+    res.json({ message: "User deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
