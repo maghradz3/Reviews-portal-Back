57bc4b057648e6d547451862d3243b6d8dbfa83e
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-09-08 13:16:19 +0400

    add reviewRoutes cruds: delete, patch get by id and post

--- models/Review.js
+++ models/Review.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
-  reviewName: String,
-  artName: String,
-  group: String,
+  name: String,
+  pieceOfArt: String,
+  group: { type: String, enum: ["Movies", "Books", "Games"] },
   tags: [String],
-  markdownText: String,
-  // ... other fields
+  reviewText: String,
+  image: String,
+  grade: { type: Number, min: 0, max: 10 },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -10,7 +10,47 @@ router.get("/reviews", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
-
 // ... Other CRUD operations for Reviews
+router.post("/reviews", async (req, res) => {
+  const review = new Review(req.body);
+  try {
+    const newReview = await review.save();
+    res.status(201).json(newReview);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.get("/reviews/:id", async (req, res) => {
+  try {
+    const review = await Review.findById(req.params.id);
+    if (!review) return res.status(404).json({ message: "Review not found" });
+    res.json(review);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.patch("/reviews/:id", async (req, res) => {
+  try {
+    const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!review) return res.status(404).json({ message: "Review not found" });
+    res.json(review);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete("/reviews/:id", async (req, res) => {
+  try {
+    const review = await Review.findByIdAndDelete(req.params.id);
+    if (!review) return res.status(404).json({ message: "Review not found" });
+    res.json({ message: "Review deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
