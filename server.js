a0177b036c186e7ba295c562d51e08b767ea58fb
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-09-11 14:35:01 +0400

    add bodyParser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 dotenv.config();
 const uri =
   process.env.MONGOOSE_URL ||
