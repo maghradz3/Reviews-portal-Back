d6d783b6a651d449ee2d39ded694366762608075
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-09-20 22:52:49 +0400

    add comment deleting logic

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { addComment } = require("../controllers/commentController");
+const {
+  addComment,
+  deleteComment,
+} = require("../controllers/commentController");
 const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post("/", authMiddleware, addComment);
-router.delete("/", authMiddleware, deteleComment);
+router.delete("/", authMiddleware, deleteComment);
 
 module.exports = router;
