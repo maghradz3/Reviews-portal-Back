e0f95e4e5300881d7bbb47e387fdd6802f0acf69
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-09-21 15:16:29 +0400

    problem with google auth

--- passportConfig.js
+++ passportConfig.js
@@ -1,56 +1,56 @@
-const passport = require("passport");
-const bcrypt = require("bcryptjs");
+// const passport = require("passport");
+// const bcrypt = require("bcryptjs");
 
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
-const User = require("./models/User");
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const FacebookStrategy = require("passport-facebook").Strategy;
+// const User = require("./models/User");
 
-// Google Strategy
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/user/auth/google/callback",
-      passReqToCallback: true,
-    },
-    async (req, accessToken, refreshToken, profile, cb) => {
-      try {
-        let user = await User.findOne({ googleId: profile.id });
+// // Google Strategy
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: "http://localhost:5000/user/auth/google/callback",
+//       passReqToCallback: true,
+//     },
+//     async (req, accessToken, refreshToken, profile, cb) => {
+//       try {
+//         let user = await User.findOne({ googleId: profile.id });
 
-        if (!user) {
-          user = new User({
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-            email: profile.emails[0].value,
-            googleId: profile.id,
-          });
-          await user.save();
-        }
+//         if (!user) {
+//           user = new User({
+//             firstName: profile.name.givenName,
+//             lastName: profile.name.familyName,
+//             email: profile.emails[0].value,
+//             googleId: profile.id,
+//           });
+//           await user.save();
+//         }
 
-        return cb(null, user);
-      } catch (err) {
-        console.error("Error in GoogleStrategy:", err);
-        return cb(err, null);
-      }
-    }
-  )
-);
+//         return cb(null, user);
+//       } catch (err) {
+//         console.error("Error in GoogleStrategy:", err);
+//         return cb(err, null);
+//       }
+//     }
+//   )
+// );
 
-passport.serializeUser((user, cb) => {
-  console.log("Serializing user:", user);
-  cb(null, user._id);
-});
+// passport.serializeUser((user, cb) => {
+//   console.log("Serializing user:", user);
+//   cb(null, user._id);
+// });
 
-passport.deserializeUser(async (id, cb) => {
-  try {
-    const user = await User.findById(id);
-    cb(null, user);
-  } catch (err) {
-    console.log("Error deserializing", err);
-    cb(err, null);
-  }
-});
+// passport.deserializeUser(async (id, cb) => {
+//   try {
+//     const user = await User.findById(id);
+//     cb(null, user);
+//   } catch (err) {
+//     console.log("Error deserializing", err);
+//     cb(err, null);
+//   }
+// });
 
 // Facebook Strategy
 // passport.use(
