cdbffd5426fa5f4863766125f3424be41298b67d
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-09-26 18:55:35 +0400

    cors headers

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
